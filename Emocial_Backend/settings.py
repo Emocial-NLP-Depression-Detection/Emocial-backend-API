9bc108b97dfc70f7d2cb20471fcc50a37e7ed3b9
Jimmy_Tempest <smartwattana@gmail.com>
2021-05-04 00:52:58 +0700

    that missing thing it need

--- Emocial_Backend/settings.py
+++ Emocial_Backend/settings.py
@@ -20,11 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from environs import Env
+from decouple import config
 
-env = Env()
-env.read_env()
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
