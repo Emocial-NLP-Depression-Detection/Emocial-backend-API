42fff72d629883f5f1163b198072b9e4221eb5d5
Jimmy_Tempest <smartwattana@gmail.com>
2021-05-04 00:57:18 +0700

    fuck open source

--- Emocial_Backend/settings.py
+++ Emocial_Backend/settings.py
@@ -20,9 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from decouple import config
-
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = 'blcz0y(c*=p#!lope%sz(-2g1x6w@krrcm7!g7j-i^+zgj+1ek'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
